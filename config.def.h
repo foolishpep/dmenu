e0e4e89062bf64fd35c0ed737746d5b6275df521
Alexander Kim <jawsoneyes@gmail.com>
2025-08-27 02:26:10 -0700

    - Edited configs

--- config.def.h
+++ config.def.h
@@ -6,14 +6,15 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"IosevkaTerm Nerd Font Propo:size=12"
+  "IosevkaTerm Nerd Font Propo:size=12",
+  "Noto Color Emoji:pixelsize=12:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#0a0a0a", "#c7c7c7" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+  /*     fg         bg       */
+  [SchemeNorm] = { "#bbbbbb", "#222222" },
+  [SchemeSel] = { "#0a0a0a", "#c7c7c7" },
+  [SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
@@ -25,4 +26,4 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 2;
+static unsigned int border_width = 0;
