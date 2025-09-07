8e3415bf92fd2d3673226e59712f82bf1c3309ed
Alexander Kim <jawsoneyes@gmail.com>
2025-09-07 12:15:39 -0700

    Edited font

--- config.def.h
+++ config.def.h
@@ -6,8 +6,8 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-  "IosevkaTerm Nerd Font Propo:size=12",
-  "Noto Color Emoji:pixelsize=12:antialias=true:autohint=true"
+  "IosevkaTerm Nerd Font:size=12",
+  "Noto Color Emoji:size=12:antialias=true:autohint=true",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
