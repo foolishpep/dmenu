cd011f3b6ed5dd74336eb788484fba4ea828609d
Alexander Kim <jawsoneyes@gmail.com>
2025-09-12 12:39:50 -0700

    Fixed make warnings

--- config.def.h
+++ config.def.h
@@ -6,12 +6,12 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 static int fuzzy  = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
+static char *fonts[] = {
 	"IosevkaTerm Nerd Font:size=12",
 	"Noto Color Emoji:size=12:antialias=true:autohint=true",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
+static char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#0a0a0a", "#c7c7c7" },
